a73fd80787f59c52b2e8910a351048220c3a47f4
flodobeutlin <flodobeutlin@mailbox.org>
2022-04-22 22:15:07 +0200

    state

--- index.js
+++ index.js
@@ -1,30 +1,107 @@
 const CANVAS_WIDTH = 1920;
 const CANVAS_HEIGHT = 1080;
-let ctx;
+let drawStyle = "waves";
+
+const styles = {
+    waves: drawWaves,
+    circles: drawCircles
+};
+
 document.addEventListener("DOMContentLoaded", init);
 
+const randomFloat = (lb, ub) => Math.random() * (ub - lb) +lb;
+const randomInt = (lb, ub) => Math.floor(randomFloat(lb,ub));
+const randomInc = (lb, ub) => randomInt(lb,ub) * ((Math.random() > 0.5) ? 1 : -1);
+
+
+function randomPalette(){
+    return {
+        hue : randomInt(0,360),
+        sat : randomInt(20,80),
+        light : randomInt(20,80),
+        lightInc : randomInc(2,16),
+        satInc : randomInc(2,16)
+    };
+}
 
 function draw(){
     const canvas = document.getElementById("canvas");
     canvas.width = CANVAS_WIDTH;
     canvas.height = CANVAS_HEIGHT;
-    ctx = canvas.getContext("2d");
+    const ctx = canvas.getContext("2d");
+    const palette = randomPalette();
+    console.log(palette);
 
-    ctx.fillStyle = "hsl(262, 100%,30%)";
+    styles[drawStyle](ctx, palette);
+
+}
+
+function drawWaves(ctx, palette) {
+    const {hue,sat,light,lightInc,satInc} = palette;
+    const startHeight = randomFloat(0, CANVAS_HEIGHT / 2);
+    const xInc = randomInc(16,128);
+    const yInc = randomInt(16,128);
+    const ampl = randomInt(32,128);
+    console.log({startHeight,xInc,yInc,ampl});
+    const segments = 200;
+    const wl = CANVAS_WIDTH / ( 5 + ( 15 * Math.random() ) );
+    ctx.fillStyle = `hsl(${hue}, ${sat}%,${light}%)`;
     ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-    const nextHeight = () => Math.random() * CANVAS_HEIGHT;
-    ctx.fillStyle = "hsl(262, 80%, 10%)";
-    ctx.beginPath();
-    const startHeight = nextHeight();
-    ctx.moveTo(0, startHeight);
-    for(let i=0; i<=5; i++){
-        ctx.lineTo(i * CANVAS_WIDTH / 5, nextHeight());
+
+    const drawLine = (offset) => {
+        ctx.fillStyle = `hsl(${hue}, ${sat + satInc * offset}%,${light + lightInc * offset}%)`;
+        ctx.beginPath();
+        const myHeight = yInc * offset + ( ampl * Math.sin( offset * xInc / wl ) );
+        ctx.moveTo(0, myHeight);
+        for(let i=0; i<=segments; i++){
+            const x = i * CANVAS_WIDTH / segments;
+            ctx.lineTo( x , myHeight + ( ampl * Math.sin( (x + xInc * offset) / wl ) ) );
+        }
+        ctx.lineTo(CANVAS_WIDTH, CANVAS_HEIGHT);
+        ctx.lineTo(0, CANVAS_HEIGHT);
+        ctx.lineTo(0, startHeight);
+        ctx.fill();    
+    };
+
+    for(let i = 0; i <= 10; i++){
+        drawLine(i);
     }
-    ctx.lineTo(CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.lineTo(0, CANVAS_HEIGHT);
-    ctx.lineTo(0, startHeight);
-    ctx.fill();
+}
+
+function drawCircles(ctx, palette){
+    const {hue,sat,light,lightInc,satInc} = palette;
+    const nCircles = randomInt(3,6);
+    const xInc = randomInc(0,32);
+    const yInc = randomInc(0,32);
+    
+    ctx.fillStyle = `hsl(${hue}, ${sat}%,${light}%)`;
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    const drawRings = () => {
+        const x = randomInt(0,CANVAS_WIDTH);
+        const y = randomInt(0,CANVAS_HEIGHT);
+        const radiusInc = randomInt(12,64);
+        const nRings = randomInt(2,10);
+        const radius = randomInt(16,128);
+        for(let i = 0; i < nRings; i++){
+            ctx.beginPath();
+            ctx.arc(x + i * xInc,y + i * yInc, radius + (nRings - i) * radiusInc, 0, 2 * Math.PI);
+            ctx.fillStyle = `hsl(${hue}, ${sat + satInc * (i + 1)}%,${light + lightInc * (i + 1)}%)`;
+            ctx.fill();
+        }
+    };
+
+    for(let i = 0; i< nCircles; i++){
+        drawRings();
+    };
+    
+}
+
+function styleChanged(){
+    const style = document.getElementById("style").value;
+    drawStyle = style;
+    draw();
 }
 
 function init(){
